513cdc7f3842846450d0063f56fc8a74d7f34019
MaierL <l.maier93@yahoo.com>
2021-10-12 14:26:25 +0200

    initial commit for a kotlin project

---
+++ src/main/kotlin/com/example/cookieRun/DecorationApplication.kt
@@ -0,0 +1,11 @@
+package com.example.cookieRun
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DecorationApplication
+
+fun main(args: Array<String>) {
+	runApplication<DecorationApplication>(*args)
+}
